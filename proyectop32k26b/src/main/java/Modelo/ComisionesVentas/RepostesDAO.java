3509bce1709ba35127e7fe0d80ff6bb5d86ea8cd
[Xander <[xreyeso@miumg.edu.gt]>
2026-05-11 20:07:50 -0600

    reseteo

--- proyectop32k26b/src/main/java/Modelo/ComisionesVentas/RepostesDAO.java
+++ proyectop32k26b/src/main/java/Modelo/ComisionesVentas/RepostesDAO.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Controlador.ComisionesVentas;
+package Modelo.ComisionesVentas;
 
 /**
  *
  * @author Angel R
  */
-public class clsReportes {
+public class RepostesDAO {
     
 }
